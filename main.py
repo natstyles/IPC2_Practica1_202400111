9f02804487822c443544226a739f3f7cb9548611
Steven Arizandieta <3817207210101@ingenieria.usac.edu.gt>
2025-08-15 17:50:10 -0600

    [ADD] Guardar libros en librerías
    - Ahora los libros guardados se guardan en sus respectivas librerias digitales y fisicas, para poder ser llamadas en cualquier momento.

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     def registrarLibro():
             print('\n------------Registrar un Libro--------------')
             print('|1. Registrar libro físico                 |')
-            print('|2. Registrara libro digital               |')
+            print('|2. Registrar libro digital                |')
             print('|3. Salir                                  |')
             print('--------------------------------------------')
 
@@ -35,8 +35,10 @@ if __name__ == "__main__":
                 id = uuid_length(8)
                 estado_prestamo = False
                 fisico = LibroFisico(titulo, autor, id, estado_prestamo, no_ejemplar)
-                print("\nLibro registrado exitosamente")
-                showInfo(fisico)
+                guardar_libroFisico(fisico)
+                print("\nLibro registrado exitosamente con el ID:", id)
+                print(libreriaFisica)
+
 
             elif opcion2 == 2:
                 titulo = input("Titulo: ")
@@ -45,8 +47,45 @@ if __name__ == "__main__":
                 id = uuid_length(8)
                 estado_prestamo = False
                 digital = LibroDigital(titulo, autor, id, estado_prestamo, tamaño_archivo)
-                print("\nLibro registrado exitosamente")
-                showInfo(digital)
+                guardar_libroDigital(digital)
+                print("\nLibro registrado exitosamente con el ID:", id)
+                print(libreriaDigital)
+
+
+            elif opcion2 == 3:
+                print("\nRegresando al menú principal...")
+                return
+            
+            else:
+                print("\nSelecciona una opción válida")
+
+    #MÉTODO PARA GUARDAR LIBROS EN BIBLIOTECA
+    libreriaFisica = {}
+    def guardar_libroFisico(fisico):
+        
+        idLibro = fisico.getID()
+
+        libreriaFisica[idLibro] = {
+            "Titulo": fisico.getTitulo(),
+            "Autor": fisico.getAutor(),
+            "Estado de prestamo": fisico.getEstadoPrestamo(),
+            "Número de ejemplar": fisico.getNoEjemplar()
+        }
+
+    libreriaDigital = {}
+    def guardar_libroDigital(digital):
+
+        idLibro = digital.getID()
+
+        libreriaDigital[idLibro] = {
+            "Titulo": digital.getTitulo(),
+            "Autor": digital.getAutor(),
+            "Estado de prestamo": digital.getEstadoPrestamo(),
+            "Número de ejemplar": digital.getTamañoArchivo()
+        }
+
+
+
 
     while True: 
         print('\n---------------Menu Biblioteca---------------')
