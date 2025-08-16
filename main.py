189342420aa96dba9b0539bbcc31a9c3f144b21c
Steven Arizandieta <3817207210101@ingenieria.usac.edu.gt>
2025-08-15 19:22:26 -0600

    [ADD] Gestionar Material
    - Se ha añadido la opción para gestionar el nuevo material ingresado al programa.
    
    - Se ha implementado la opción para poder prestar libros.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #IMPORTS
 from LibroDigital import LibroDigital
 from LibroFisico import LibroFisico
+from pprint import pprint
 import uuid
 
 #PLIMORFISMO
@@ -11,11 +12,12 @@ def showInfo(MaterialBiblioteca):
 def uuid_length(longitud):
     return uuid.uuid4().hex[:longitud]
 
-#Interfaz inicial
+
 if __name__ == "__main__":
     
     digital = None
     fisico = None
+    tiempoDevolucion = None
 
     #MÉTODO PARA REGISTRAR LIBROS
     def registrarLibro():
@@ -81,16 +83,76 @@ if __name__ == "__main__":
             "Titulo": digital.getTitulo(),
             "Autor": digital.getAutor(),
             "Estado de prestamo": digital.getEstadoPrestamo(),
-            "Número de ejemplar": digital.getTamañoArchivo()
+            "Peso de archivo": digital.getTamañoArchivo()
         }
 
+    #MÉTODO PARA GESTIONAR PRESTAMOS
+    def gestionLibro():
+        print('\n------------Gestionar material--------------')
+        print('|1. Prestar material                       |')
+        print('|2. Devolver material prestado             |')
+        print('|3. Salir                                  |')
+        print('--------------------------------------------')
 
+        opcion3 = int(input("\nSelecciona una opción: "))
 
+        if opcion3 == 1:
+            prestarLibros()
 
+        elif opcion3 == 2:
+            print("Devolver material")
+
+        elif opcion3 == 3:
+            print("\nRegresando al menú principal...")
+            return
+            
+        else:
+            print("\nSelecciona una opción válida")
+
+    #MÉTODO PARAA PRESTAR LIBROS
+    def prestarLibros():
+        print("\nMostrando libros disponibles:")
+        print("\nLibreria Física: ")
+        pprint(libreriaFisica)
+        print("\nLibrería Digital: ")
+        pprint(libreriaDigital)
+
+        idPrestar = input("\nEscibe el ID del libro a prestar: ")
+
+        if idPrestar in libreriaFisica:
+            tiempoDevolucion = 7
+
+            if not libreriaFisica[idPrestar]["Estado de prestamo"]:
+                tiempoUso = int(input(f"\nEscribe cuantos días lo utilizarás (MAX. {tiempoDevolucion} DÍAS): "))
+                if tiempoUso <= tiempoDevolucion:
+                    libreriaFisica[idPrestar]["Estado de prestamo"] = True
+                    print(f"\nEl libro con ID {idPrestar} ha sido prestado por {tiempoUso} días!")
+                else:
+                    print(f"\nNo puedes llevarte el libro por más de {tiempoDevolucion} días.")
+            else:
+                print("\nEl libro físico ya está prestado")
+
+        elif idPrestar in libreriaDigital:
+            tiempoDevolucion = 3
+
+            if not libreriaDigital[idPrestar]["Estado de prestamo"]:
+                tiempoUso = int(input(f"\nEscribe cuantos días lo utilizarás (MAX. {tiempoDevolucion} DÍAS): "))
+                if tiempoUso <= tiempoDevolucion:
+                    libreriaDigital[idPrestar]["Estado de prestamo"] = True
+                    print(f"\nEl libro con ID {idPrestar} ha sido prestado por {tiempoUso} días!")
+                else:
+                    print(f"\nNo puedes llevarte el libro por más de {tiempoDevolucion} días.")
+            else:
+                print("\nEl libro digital ya está prestado")
+
+        else:
+            print(f'\nEl libro con ID "{idPrestar}" no existe en ninguna librería')
+
+    #Interfaz inicial
     while True: 
-        print('\n---------------Menu Biblioteca---------------')
+        print('\n---------------Menu Biblioteca--------------')
         print('|1. Registrar nuevo material               |')
-        print('|2. Gestionar Préstamo                     |')
+        print('|2. Gestionar material registrado          |')
         print('|3. Salir                                  |')
         print('--------------------------------------------')
 
@@ -99,12 +161,9 @@ if __name__ == "__main__":
         if opcion == 1:
             registrarLibro()
         elif opcion == 2:
-            #gestionLibro()
-            print("gestion libro")
+            gestionLibro()
         elif opcion == 3:
             print("\nSaliendo del programa...")
             break
         else:
             print("\nSelecciona una opción válida")
-
-    
